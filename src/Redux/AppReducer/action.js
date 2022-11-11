8aba7c0934f21e49809b113659307cbcbc621a02
Umesh Yadav <80197485+umeshyadav4321@users.noreply.github.com>
2022-11-11 10:58:13 +0530

    json server deployed link

--- src/Redux/AppReducer/action.js
+++ src/Redux/AppReducer/action.js
@@ -2,7 +2,7 @@ import * as types from "./actionType"
 import axios from "axios";
 const getProducts=(params)=>(dispatch)=>{
     dispatch({type:types.GET_PRODUCTS_DATA_REQUEST})
-    return axios("http://localhost:8081/products",params).then(r=>{
+    return axios("https://pure-castle-49564.herokuapp.com/products",params).then(r=>{
         return dispatch({type:types.GET_PRODUCTS_DATA_SUCCESS,payload:r.data})
     }).catch(e=>{
         return dispatch({types:types.GET_PRODUCTS_DATA_FAILURE})
